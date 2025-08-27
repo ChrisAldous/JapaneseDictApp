35bdba0d6f1ee7fd19c9dadb31760339917a5259
ChrisA <christopherthealdous@gmail.com>
2025-08-26 20:24:29 -0600

    added readings, definitions, and romaji readings to my definitions page. Created a widget called fixedLabeledRow to minimize code.

--- lib/screens/word_definition_screen.dart
+++ lib/screens/word_definition_screen.dart
@@ -11,7 +11,11 @@ class WordDefinitionScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final String kanji_word = the_word.words[0].word;
     final String kana_word = the_word.words[0].reading;
+    final List<String> definitions = the_word.definition;
     final KanaKit kanaKit = KanaKit();
+    // print(the_word);
+    // print("-----------");
+    // print(the_word.definition);
 
     for (JapaneseList obj in the_word.words) {
       print(obj);
@@ -22,37 +26,66 @@ class WordDefinitionScreen extends StatelessWidget {
         preferredSize: Size.fromHeight(50),
         child: AppBar(backgroundColor: const Color.fromARGB(255, 210, 24, 11)),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Text(
                 (kanji_word != null && kanji_word.isNotEmpty)
                     ? kanji_word
                     : kana_word,
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              fixedLabeledRow('Hiragana', kana_word),
-              fixedLabeledRow('Romaji', kanaKit.toRomaji(kana_word)),
-            ],
-          ),
+            ),
+
+            fixedLabeledRow('Kana', kana_word),
+            fixedLabeledRow('Romaji', kanaKit.toRomaji(kana_word)),
+
+            Text(
+              'Meanings:',
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                definitions.map((word) => '$word').join(', '),
+                style: TextStyle(fontSize: 16,), 
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 
   Widget fixedLabeledRow(String label, String content) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-          width: 100,
-          child: Text(label, textAlign: TextAlign.left,),
-        ),
-        Flexible(child: Text(content))
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 100,
+            child: Text(
+              label,
+              textAlign: TextAlign.left,
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+          ),
+
+          const SizedBox(width: 16),
+
+          Expanded(
+            child: Text(
+              content,
+              textAlign: TextAlign.right,
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
